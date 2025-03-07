64fb8699239b5106113adcd4735a945267771eb1
Eugene Che <36330890+Hardbow@users.noreply.github.com>
2025-03-07 15:29:20 +0300

    added functional about determine and write the execution time of function

--- trnsc_utils.py
+++ trnsc_utils.py
@@ -1,29 +1,26 @@
 import whisper
 from pathlib import Path
 import os
-
+import time
 
 os.environ["PATH"] += r";C:\path\to\ffmpeg\bin"
 
 folder_name = Path(os.path.dirname(os.path.abspath(__file__)))
 file_path = folder_name / "handling.m4a"
 
-model = whisper.load_model("large")
+model = whisper.load_model("medium")
 
-#result = model.transcribe("audio.mp3", temperature=0.1)
-# result = model.transcribe("audio.mp3", beam_size=10) default beam_size=5
-# result = model.transcribe("audio.mp3", word_timestamps=True)
-# result = model.transcribe("audio.mp3", language="ru")  # Russian
-
-
+start_time = time.time()
 result = model.transcribe(str(file_path),
-                           temperature=0.1,
-                           beam_size=15,
-                           word_timestamps=True,
-                           language="ru")
+                          temperature=0.1,
+                          beam_size=15,
+                          word_timestamps=True,
+                          language="ru")
+end_time = time.time()
 
-with open("trnsc_result_large1.txt", "w") as f_write:
+with open("trnsc_result_medium2.txt", "w") as f_write:
     f_write.write(result["text"])
+    f_write.write(f"\ntime: {round(end_time - start_time, 1)} sec")
 
 for i in result["text"].split('.'):
     print(i)
